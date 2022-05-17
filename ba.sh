d29561613b091443d3044ade4f3c50b009f5ab64
DevanaskhanOps <anaskhansqa@gmail.com>
2022-05-17 17:54:00 +0500

    release to si

---
+++ ba.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ echo `git config --global user.name DevanaskhanOps`
+ echo `git config --global user.email anaskhansqa@gmail.com`
+ echo `git diff --staged --quiet || git commit -m 'ssd' `
